867275563c5c4280b1869ddbc536dc1b4d0118b8
lnx2000 <omkarsamilkanthwar2000@gmail.com>
2021-06-25 13:23:40 +0530

    Firebase connection

---
+++ Android/DEFEND/app/src/main/java/com/app/defend/activities/ChatsDashboardActivity.java
@@ -0,0 +1,16 @@
+package com.app.defend.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.defend.R;
+
+public class ChatsDashboardActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_chats_dashboard);
+	}
+}
